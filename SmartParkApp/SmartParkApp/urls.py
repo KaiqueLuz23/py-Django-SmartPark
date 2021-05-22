5211a8a03abc55160da8cbb2e0877a29e18b8a65
Kaique Luz <kakaluzrda@gmail.com>
2021-05-22 14:54:55 -0300

    commit ind

--- SmartParkApp/SmartParkApp/urls.py
+++ SmartParkApp/SmartParkApp/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
+from django.views.generic import RedirectView
+from polls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home),
+    path("", RedirectView.as_view(url='/home/'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()

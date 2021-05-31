a42082c0917805f17cb312534ad07a872fc7abe8
Kaique Luz <kakaluzrda@gmail.com>
2021-05-30 21:17:00 -0300

    commit reforme

--- SmartParkApp/SmartParkApp/urls.py
+++ SmartParkApp/SmartParkApp/urls.py
@@ -22,6 +22,7 @@ from polls import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home),
+    path('estacionando', views.estacionando),
     path('home/planos/', views.planos),
     path('login/', views.login),
     path('home1/', views.privhome),
--- SmartParkApp/polls/views.py
+++ SmartParkApp/polls/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def estacionando(request):
+    return render(request, 'index.html')
+
 def planos(request):
     return render(request, 'planos.html')
 
